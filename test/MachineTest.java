73d876be750fcb9a92ba8a7c5c01232d1876d6f9
sofia <sofiamcnary@gmail.com>
2025-02-06 18:54:43 -0700

    added code to read,write, store and load

--- test/MachineTest.java
+++ test/MachineTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MachineTest {
@@ -24,20 +26,31 @@ class MachineTest {
     @Test
     void loadTest(){
         Machine a = new Machine();
-        a.read(1001);
+        a.load(1001);
+        Assertions.assertEquals(1001, a.accumulator);
 
     }
     @Test
     void storeTest(){
+        Machine a = new Machine();
+        a.accumulator = 2500;
+        a.store(1001);
+        Assertions.assertEquals(a.accumulator, a.memory.getWordSingle(1001));
 
+        a.accumulator = 0;
+        a.store(1002);
+        Assertions.assertEquals(a.accumulator, a.memory.getWordSingle(1002));
     }
     @Test
-    void readTest(){
-
+    void readTest(){// doesnt work console is read only
+        Machine a = new Machine();
+        a.read(1001);
     }
     @Test
-    void writeTest(){
-
+    void writeTest(){//cant create a test for write without a return type
+        Machine a = new Machine();
+        a.write(1001);
+        Assertions.assertEquals(1001,1001);
     }
 
 }
